05b40b66c7d69b196fb6f872b1164c26fd3fedd4
Dan Davison <dandavison7@gmail.com>
2019-06-30 10:26:40 -0400

    Revert "DNC: try `impl Iterator<Item = impl Borrow<str>>`"
    This reverts commit affdd55868083077613207aef728e7d73689ad40.

--- src/ok_generic.rs
+++ src/ok_generic.rs
@@ -1,9 +1,8 @@
-use std::borrow::Borrow;
 use std::io::{self, BufRead};
 
 /// We want a generic function that takes any type that implements the
 /// iterator interface and yields printable items.
-fn process_lines(lines: impl Iterator<Item = impl Borrow<str>>) {
+fn process_lines(lines: impl Iterator<Item = String>) {
     for line in lines {
         println!("ok {}", line);
     }
@@ -11,5 +10,5 @@ fn process_lines(lines: impl Iterator<Item = impl Borrow<str>>) {
 
 pub fn main() {
     process_lines(io::stdin().lock().lines().map(|l| l.unwrap()));
-    process_lines("line one\nline two\n".split("\n"));
+    process_lines("line one\nline two\n".split("\n").map(String::from));
 }
