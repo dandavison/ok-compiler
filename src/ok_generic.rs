b83108708bfd9c55dab9929370fac435f23c0660
Dan Davison <dandavison7@gmail.com>
2019-06-30 10:00:06 -0400

    DNC: generic

--- src/ok_generic.rs
+++ src/ok_generic.rs
@@ -1,13 +1,15 @@
 use std::io::{self, BufRead};
 
-fn process_lines() -> io::Result<()> {
-    for line in io::stdin().lock().lines() {
-        println!("ok {}", line?);
+/// We want a generic function that takes any type that implements the
+/// iterator interface and yields printable items.
+fn process_lines(lines: impl Iterator) -> io::Result<()> {
+    for line in lines {
+        println!("ok {}", line);
     }
     Ok(())
 }
 
 
 pub fn main() {
-    process_lines().unwrap();
+    process_lines(io::stdin().lock().lines()).unwrap();
 }
