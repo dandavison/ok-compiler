d684c43b6919e772179ad3a2c2ef184661f37f51
Dan Davison <dandavison7@gmail.com>
2019-06-30 10:26:09 -0400

    DNC: try `impl Iterator<Item = impl Borrow<str>>`

--- src/ok_generic.rs
+++ src/ok_generic.rs
@@ -1,8 +1,9 @@
+use std::borrow::Borrow;
 use std::io::{self, BufRead};
 
 /// We want a generic function that takes any type that implements the
 /// iterator interface and yields printable items.
-fn process_lines(lines: impl Iterator<Item = String>) {
+fn process_lines(lines: impl Iterator<Item = impl Borrow<str>>) {
     for line in lines {
         println!("ok {}", line);
     }
@@ -10,5 +11,5 @@ fn process_lines(lines: impl Iterator<Item = String>) {
 
 pub fn main() {
     process_lines(io::stdin().lock().lines().map(|l| l.unwrap()));
-    process_lines("line one\nline two\n".split("\n").map(String::from));
+    process_lines("line one\nline two\n".split("\n"));
 }
