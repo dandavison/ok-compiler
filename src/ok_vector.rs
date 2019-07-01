fc4c4c1737838a2335569bf44e0d99f49ce27731
Dan Davison <dandavison7@gmail.com>
2019-07-01 18:36:54 -0400

    ok_vector: fix

--- src/ok_vector.rs
+++ src/ok_vector.rs
@@ -11,12 +11,12 @@ fn f2() {
 }
 
 fn f3() {
-    let mut array_of_mut_vecs_of_strings: [Vec<String>; 2] = [Vec::new(), Vec::new()]; // warning: variable does not need to be mutable
-    append(array_of_mut_vecs_of_strings); // value moved here
-    println!("{:?}", array_of_mut_vecs_of_strings); // error[E0382]: borrow of moved value: `array_of_mut_vecs_of_strings`
+    let mut array_of_mut_vecs_of_strings: [Vec<String>; 2] = [Vec::new(), Vec::new()];
+    append(&mut array_of_mut_vecs_of_strings);
+    println!("{:?}", array_of_mut_vecs_of_strings);
 }
 
-fn append(mut a: [Vec<String>; 2]) {
+fn append(a: &mut [Vec<String>; 2]) {
     a[1].push(String::from("Play"));
 }
 
