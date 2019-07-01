02eaad68cc962fd4c72683502e459b123f27b617
Dan Davison <dandavison7@gmail.com>
2019-07-01 18:46:45 -0400

    ok_vector add tuple version

--- src/ok_vector.rs
+++ src/ok_vector.rs
@@ -12,16 +12,28 @@ fn f2() {
 
 fn f3() {
     let mut array_of_mut_vecs_of_strings: [Vec<String>; 2] = [Vec::new(), Vec::new()];
-    append(&mut array_of_mut_vecs_of_strings);
+    append_array(&mut array_of_mut_vecs_of_strings);
     println!("{:?}", array_of_mut_vecs_of_strings);
 }
 
-fn append(a: &mut [Vec<String>; 2]) {
+fn append_array(a: &mut [Vec<String>; 2]) {
     a[1].push(String::from("Play"));
+}
+
+fn f4() {
+    let mut tuple_of_mut_vecs_of_strings = (Vec::new(), Vec::new());
+    append_tuple(&mut tuple_of_mut_vecs_of_strings);
+    println!("{:?}", tuple_of_mut_vecs_of_strings);
+}
+
+fn append_tuple(pair: &mut (Vec<String>, Vec<String>)) {
+    let (_, v2) = pair;
+    v2.push(String::from("Play"));
 }
 
 pub fn main() {
     f1();
     f2();
     f3();
+    f4();
 }
