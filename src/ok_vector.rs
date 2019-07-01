b835d4da1009a8bfec6cc1da9d11b03f9ee092cf
Dan Davison <dandavison7@gmail.com>
2019-07-01 11:59:19 -0400

    DNC passing array of vectors of Strings

--- src/ok_vector.rs
+++ src/ok_vector.rs
@@ -10,7 +10,18 @@ fn f2() {
     println!("{:?}", array_of_mut_vecs_of_strings);
 }
 
+fn f3() {
+    let mut array_of_mut_vecs_of_strings: [Vec<String>; 2] = [Vec::new(), Vec::new()]; // warning: variable does not need to be mutable
+    append(array_of_mut_vecs_of_strings); // value moved here
+    println!("{:?}", array_of_mut_vecs_of_strings); // error[E0382]: borrow of moved value: `array_of_mut_vecs_of_strings`
+}
+
+fn append(mut a: [Vec<String>; 2]) {
+    a[1].push(String::from("Play"));
+}
+
 pub fn main() {
     f1();
     f2();
+    f3();
 }
