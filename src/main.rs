5f49f33a7935d989609674683cddad06b2f359a3
Dan Davison <dandavison7@gmail.com>
2019-07-03 17:16:48 -0400

    ok_syntect

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod ok_generic;
 mod ok_reference;
 mod ok_struct;
+mod ok_syntect;
 mod ok_vector;
 
 
@@ -9,4 +10,5 @@ fn main() {
     ok_reference::main();
     ok_generic::main();
     ok_vector::main();
+    ok_syntect::main();
 }
