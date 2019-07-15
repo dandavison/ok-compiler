24ff3a1fadde10827ac9af490900461c69708f55
Dan Davison <dandavison7@gmail.com>
2019-07-14 21:25:57 -0400

    ok_syntect: python

--- src/ok_syntect.rs
+++ src/ok_syntect.rs
@@ -7,16 +7,17 @@ use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 fn print(lang: &str, code: &str) {
     // Load these once at the start of your program
-    let ps = SyntaxSet::load_defaults_newlines();
-    let ts = ThemeSet::load_defaults();
+    let syntax_set = SyntaxSet::load_defaults_newlines();
+    let theme_set = ThemeSet::load_defaults();
 
-    let syntax = ps.find_syntax_by_extension(lang).unwrap();
-    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.light"]);
+    let syntax = syntax_set.find_syntax_by_extension(lang).unwrap();
+    let mut h = HighlightLines::new(syntax, &theme_set.themes["base16-ocean.light"]);
     for line in LinesWithEndings::from(code) {
-        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &syntax_set);
         let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
-        println!("{}", escaped);
+        print!("{}", escaped);
     }
+    println!("\x1b[0m");
 }
 
 pub fn main() {
@@ -28,4 +29,13 @@ pub struct Wow { hi: u64 }
 fn blah() -> u64 {}
 ",
     );
+    print(
+        "py",
+        "
+# hello
+def f():
+    1
+",
+    );
+    print!("\n\n");
 }
