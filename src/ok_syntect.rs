79d7e2773fb1ab82bb93a8d45b4f86634647452d
Dan Davison <dandavison7@gmail.com>
2019-07-14 21:20:40 -0400

    ok_syntect: rust

--- src/ok_syntect.rs
+++ src/ok_syntect.rs
@@ -4,17 +4,28 @@ use syntect::parsing::SyntaxSet;
 use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 // https://github.com/trishume/syntect#example-code
-pub fn main() {
+
+fn print(lang: &str, code: &str) {
     // Load these once at the start of your program
     let ps = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
 
-    let syntax = ps.find_syntax_by_extension("rs").unwrap();
-    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
-    let s = "pub struct Wow { hi: u64 }\nfn blah() -> u64 {}";
-    for line in LinesWithEndings::from(s) {
+    let syntax = ps.find_syntax_by_extension(lang).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.light"]);
+    for line in LinesWithEndings::from(code) {
         let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
         let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
         println!("{}", escaped);
     }
+}
+
+pub fn main() {
+    print(
+        "rs",
+        "
+// hello
+pub struct Wow { hi: u64 }
+fn blah() -> u64 {}
+",
+    );
 }
