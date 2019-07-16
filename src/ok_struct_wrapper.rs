0e48897962bc590c8248aa242388603f37bc51ce
Dan Davison <dandavison7@gmail.com>
2019-07-15 21:30:00 -0400

    Simplify ok_struct_wrapper

--- src/ok_struct_wrapper.rs
+++ src/ok_struct_wrapper.rs
@@ -1,16 +1,16 @@
 use std::fmt;
 
-struct StrWrapper<'a>(&'a str);
+struct StringWrapper(String);
 
-impl fmt::Display for StrWrapper<'static> {
+impl fmt::Display for StringWrapper {
     fn fmt(&self, writer: &mut fmt::Formatter) -> fmt::Result {
         writeln!(writer, "{}", self.0)
     }
 }
 
 pub fn main() {
-    let f = StrWrapper {
-        0: "this should be printed",
+    let f = StringWrapper {
+        0: "this should be printed".to_string(),
     };
     println!("f: {}", f);
 }
