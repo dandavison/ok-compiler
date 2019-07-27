22b65d18a13f2463018ef9f4516312e7693b518d
Dan Davison <dandavison7@gmail.com>
2019-07-27 14:08:00 -0400

    ok_indexing

--- src/ok_indexing.rs
+++ src/ok_indexing.rs
@@ -2,4 +2,8 @@ pub fn main() {
     let s = String::from("0123456789");
     println!("s[0..7] = {}", &s[0..7]);
     println!("s[7..9] = {}", &s[7..9]);
+    let s = String::from("0");
+    println!("s[1..] = {}", &s[1..]);
+    let s = vec![7];
+    println!("s[1..] = {:?}", &s[1..]);
 }
