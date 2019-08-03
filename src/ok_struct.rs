96b178699affdb499ec26652964bc306be0b75cf
Dan Davison <dandavison7@gmail.com>
2019-08-03 16:22:58 -0400

    Demonstrate that the same struct cannot be pushed twice into a vec

--- src/ok_struct.rs
+++ src/ok_struct.rs
@@ -1,3 +1,5 @@
+use syntect::highlighting::StyleModifier;
+
 #[derive(Debug)] // so it can be formatted with {:?}
 struct MyStruct {
     k1: usize,
@@ -11,7 +13,41 @@ fn f1() -> MyStruct {
     }
 }
 
+fn make_vec<'a, T>(x: T) -> Vec<Vec<(T, &'a str)>> {
+    let mut v = Vec::new();
+    v.push(vec![(x, "a")]);
+    // v.push(vec![(x, "b")]);
+    v
+}
+
 pub fn main() {
-    let my_struct = f1();
-    println!("{:?}", my_struct);
+    {
+        let x = f1();
+        let mut v = Vec::new();
+        v.push(x);
+        println!("{:?}", v);
+        // v.push(x); // use of moved value: `x`
+        // println!("{:?}", v);
+    }
+    {
+        let x = f1();
+        let mut v = Vec::new();
+        v.push(vec![x]);
+        println!("{:?}", v);
+        // v.push(vec![x]); // use of moved value: `x`
+        // println!("{:?}", v);
+    }
+    {
+        let x = f1();
+        let mut v = Vec::new();
+        v.push(vec![(x, 1)]);
+        println!("{:?}", v);
+        // v.push(vec![(x, 2)]); // use of moved value: `x`
+        // println!("{:?}", v);
+    }
+    {
+        let x = StyleModifier::default();
+        let v = make_vec(x);
+        println!("{:?}", v);
+    }
 }
