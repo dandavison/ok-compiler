8dcd24562104ef25f44847ffd478a7c12a486218
Dan Davison <dandavison7@gmail.com>
2019-08-10 14:30:20 -0700

    ok_iterator_min 2

--- src/ok_iterator_min.rs
+++ src/ok_iterator_min.rs
@@ -1,11 +1,12 @@
+#[derive(Clone)]
 struct X {
     a: usize,
 }
 
 fn get_minimal_structs() -> Vec<X> {
     let mut selection = Vec::<X>::new();
-    let mut candidates = vec![X { a: 1 }, X { a: 2 }];
-    selection.push(candidates.drain(..).min_by_key(|x| x.a).unwrap());
+    let candidates = [X { a: 1 }, X { a: 2 }];
+    selection.push((*candidates.iter().min_by_key(|x| x.a).unwrap()).clone());
     selection
 }
 
