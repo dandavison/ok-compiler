5df396f8f76ac4b1b228fb36023badff01098518
Dan Davison <dandavison7@gmail.com>
2019-09-16 12:14:05 +0100

    ok_string

--- src/ok_string.rs
+++ src/ok_string.rs
@@ -1,5 +1,13 @@
+fn use_alphabet(alphabet: &[char]) {
+    println!("{:?}", alphabet[1]);
+}
+
 pub fn main() {
     let s = "aaa  \n  ";
     println!("__{}__", s);
     println!("__{}__", s.trim_end());
+    println!("{:?}", s.chars().nth(1));
+    let alphabet: [char; 3] = ['a', 'b', 'c'];
+    println!("{:?}", alphabet[1]);
+    use_alphabet(&alphabet);
 }
