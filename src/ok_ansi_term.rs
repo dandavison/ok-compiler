0222c6e5be5dcca92af34d00a77bdb46eee605a1
Dan Davison <dandavison7@gmail.com>
2020-02-25 21:36:35 -0600

    ok_ansi_term

--- src/ok_ansi_term.rs
+++ src/ok_ansi_term.rs
@@ -1,7 +1,12 @@
 extern crate ansi_term;
-use ansi_term::Colour::{Black, Blue, Cyan, Fixed, Green, Purple, Red, Yellow};
-use ansi_term::Style;
+use ansi_term;
+use ansi_term::Colour::{Blue, Yellow};
 
 pub fn main() {
+    let mut ansi_term_style = match Blue {
+        style::NO_COLOR => ansi_term::Style::default(),
+        color => to_ansi_color(color).normal(),
+    };
+
     println!("{}", Blue.on(Yellow).paint("Blue on yellow!"));
 }
