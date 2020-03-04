95dfde65f21aedb95ce3086bfa5f5aa2277a0e0b
Dan Davison <dandavison7@gmail.com>
2020-03-03 23:28:19 -0600

    generic type and non-generic type struct members

--- src/ok_generic_struct_with_lifetime.rs
+++ src/ok_generic_struct_with_lifetime.rs
@@ -1,11 +1,15 @@
 struct MyStruct<'a, T> {
-    my_ref: &'a T,
+    my_u8: &'a u8,
+    my_t: T,
 }
 
 fn f1() {
     let n: u8 = 77;
-    let my_struct = MyStruct::<u8> { my_ref: &n };
-    println!("{}", my_struct.my_ref);
+    let my_struct = MyStruct::<String> {
+        my_t: "s".to_string(),
+        my_u8: &n,
+    };
+    println!("{} {}", my_struct.my_t, my_struct.my_u8);
 }
 
 pub fn main() {
