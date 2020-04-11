e589f4a6d03f4f3755b3e2819a60acb931ecc7e4
Dan Davison <dandavison7@gmail.com>
2020-04-11 12:04:06 -0400

    ok_iterator_advance

--- src/ok_iterator_advance.rs
+++ src/ok_iterator_advance.rs
@@ -1,11 +1,23 @@
 use unicode_segmentation::UnicodeSegmentation;
 
-pub fn main() {
-    {
-        let mut line = "aábcdefghijklmn".graphemes(true);
-        let prefix = line.next().unwrap(); // we can see that this won't panic
+fn prepare(line: &str) -> String {
+    if !line.is_empty() {
+        let mut line = line.graphemes(true);
+        let prefix = line.next().unwrap();
         let output_line = line.collect::<String>();
-        println!("{}", prefix);
-        println!("{}", output_line);
+        format!("{}{}", prefix, output_line)
+    } else {
+        "<empty line>".to_string()
     }
+}
+
+pub fn main() {
+    let result = prepare("aábcdefghijklmn");
+    println!("{}", result);
+    // This example does work: it prints aábcdefghijklmn.
+
+    // However, when calling a function like prepare in the context of a larger project, the string
+    // returned from prepare is sometimes truncated on the right-hand side, in apparently
+    // unpredicatble locations, but always word boundaries of some sort. What mistake might I be
+    // making?
 }
