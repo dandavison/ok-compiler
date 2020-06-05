a60f33d888585f64de39010ba61f7631d49be57b
Dan Davison <dandavison7@gmail.com>
2020-06-05 09:50:04 -0400

    DNW: additional rule to handle trailing comma

--- src/ok_macros.rs
+++ src/ok_macros.rs
@@ -10,10 +10,15 @@ macro_rules! set_fields {
             $the_struct.$field_name = $val;
         )*
     };
+    // Additional rule to handle trailing comma after last tuple inside [...]
+    ([$( $tuple:expr ),* ,], $the_struct:expr) => {
+        set_fields!([$( $tuple ),*], $the_struct)
+    };
 }
 
 pub fn main() {
     let mut s = S::default();
-    set_fields!([(field_1, "val_1"), (field_2, "val_2")], &mut s);
+    set_fields!([(field_1, "val_1"), (field_2, "val_2")], &mut s); // OK
+    set_fields!([(field_1, "val_1"), (field_2, "val_2"),], &mut s); // error: no rules expected the token `]`
     println!("{:?}", s);
 }
