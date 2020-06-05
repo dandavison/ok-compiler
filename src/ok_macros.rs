e4c298c767a6ce5f576741b3c23afdb145ca3803
Dan Davison <dandavison7@gmail.com>
2020-06-05 11:29:54 -0400

    Fix trailing-comma rule of macro

--- src/ok_macros.rs
+++ src/ok_macros.rs
@@ -11,14 +11,15 @@ macro_rules! set_fields {
         )*
     };
     // Additional rule to handle trailing comma after last tuple inside [...]
-    ([$( $tuple:expr ),* ,], $the_struct:expr) => {
-        set_fields!([$( $tuple ),*], $the_struct)
+    ([$(($field_name:ident, $val:expr)),* ,], $the_struct:expr) => {
+        set_fields!([$( ($field_name, $val) ),*], $the_struct)
     };
 }
 
 pub fn main() {
     let mut s = S::default();
-    set_fields!([(field_1, "val_1"), (field_2, "val_2")], &mut s); // OK
-    set_fields!([(field_1, "val_1"), (field_2, "val_2"),], &mut s); // error: no rules expected the token `]`
+    set_fields!([(field_1, "val_11"), (field_2, "val_21")], &mut s);
+    println!("{:?}", s);
+    set_fields!([(field_1, "val_12"), (field_2, "val_22"),], &mut s);
     println!("{:?}", s);
 }
