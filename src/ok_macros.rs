7bea4a594d4b1844e4c982ff8284be4a01f87776
Dan Davison <dandavison7@gmail.com>
2020-06-05 08:58:25 -0400

    macro with repetition

--- src/ok_macros.rs
+++ src/ok_macros.rs
@@ -1,10 +1,9 @@
 macro_rules! process_tuples {
-    ([($field_11:expr, $field_12:expr), ($field_21:expr, $field_22:expr)]) => {
-        println!("field_11 = {}, field_12 = {}", $field_11, $field_12);
-        println!("field_21 = {}, field_22 = {}", $field_21, $field_22);
+    ([$(($field_1:expr, $field_2:expr)),* ], $arg2:expr) => {
+        $(println!("field_1 = {}, field_2 = {}, arg2 = {}", $field_1, $field_2, $arg2);)*
     };
 }
 
 pub fn main() {
-    process_tuples!([("a11", "a12"), ("a21", "a22")]);
+    process_tuples!([("a11", "a12"), ("a21", "a22")], "hello");
 }
