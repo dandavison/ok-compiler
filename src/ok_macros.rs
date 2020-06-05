7556891e909b230263ad30ea8a0b50eac1cfe0c7
Dan Davison <dandavison7@gmail.com>
2020-06-05 08:45:33 -0400

    ok_macros

--- src/ok_macros.rs
+++ src/ok_macros.rs
@@ -1,9 +1,10 @@
 macro_rules! process_tuples {
-    ([($field_1:expr, $field_2:expr)]) => {
-        println!("field_1 = {}, field_2 = {}", $field_1, $field_2);
+    ([($field_11:expr, $field_12:expr), ($field_21:expr, $field_22:expr)]) => {
+        println!("field_11 = {}, field_12 = {}", $field_11, $field_12);
+        println!("field_21 = {}, field_22 = {}", $field_21, $field_22);
     };
 }
 
 pub fn main() {
-    process_tuples!([("a11", "a12")]);
+    process_tuples!([("a11", "a12"), ("a21", "a22")]);
 }
