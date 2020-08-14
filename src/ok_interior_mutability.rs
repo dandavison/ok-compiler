1b2ce7de305c11efa068da19570fde60ee14cd1b
Dan Davison <dandavison7@gmail.com>
2020-08-13 22:03:14 -0400

    ok_interior_mutability

---
+++ src/ok_interior_mutability.rs
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////
+// Start 3rd-party library
+//
+struct StateMachine;
+
+impl StateMachine {
+    // The 3rd-party library dictates that self is a mutable reference.
+    fn notify_client(&mut self, client: &mut dyn Client) {
+        client.mutate();
+    }
+}
+
+trait Client {
+    fn mutate(&mut self);
+}
+
+//
+// End 3rd-party library
+/////////////////////////////////////////////////////
+
+struct MyThing {
+    state_machine_from_3rd_party_lib: StateMachine,
+    data: usize,
+}
+
+impl MyThing {
+    fn advance_state_machine(&mut self) {
+        // cannot borrow `self.state_machine_from_3rd_party_lib` as mutable more than once at a time
+        // cannot borrow `*self` as mutable more than once at a time
+        self.state_machine_from_3rd_party_lib.notify_client(self)
+    }
+}
+
+impl Client for MyThing {
+    fn mutate(&mut self) {
+        self.data += 1;
+    }
+}
+
+pub fn main() {
+    let mut thing = MyThing {
+        state_machine_from_3rd_party_lib: StateMachine {},
+        data: 0,
+    };
+    thing.advance_state_machine();
+    println!("{}", thing.data);
+}
